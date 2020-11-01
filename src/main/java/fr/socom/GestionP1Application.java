e1e6fe97bc980754c9678363d1522a298797f3f3
dorolyci4 <dorolyci4@gmail.com>
2020-11-01 18:40:56 -0500

    First Commit

---
+++ src/main/java/fr/socom/GestionP1Application.java
@@ -0,0 +1,13 @@
+package fr.socom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionP1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestionP1Application.class, args);
+	}
+
+}
---
+++ src/test/java/fr/socom/GestionP1ApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.socom;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestionP1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
